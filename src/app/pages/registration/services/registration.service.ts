515a8ec29c60e2879d5939141c284c8eadf29491
berdikulov-571 <mygithub571@gmail.com>
2024-01-28 20:43:22 +0500

    service ts

--- src/app/pages/registration/services/registration.service.ts
+++ src/app/pages/registration/services/registration.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CreateUserCommand } from '../registration.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  private baseUrl = 'https://localhost:7148/api/User/';
+
+  createUser(data: CreateUserCommand): Observable<CreateUserCommand> {
+    return this.httpClient.post<CreateUserCommand>(this.baseUrl + 'Create', data);
+  }
+
 }
