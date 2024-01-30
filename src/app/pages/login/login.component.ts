1fe01624d90db84b60cbe2777fd6aa787d3b7319
berdikulov-571 <mygithub571@gmail.com>
2024-01-30 23:54:38 +0500

    login component ts

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginService } from './service/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private loginService: LoginService) { }
+
+  loginUser() {
+    let emailInput = document.querySelector('.email') as HTMLInputElement;
+    let passwordInput = document.querySelector('.password') as HTMLInputElement;
+
+    let email = emailInput.value;
+    let password = passwordInput.value;
+
+    let data: LoginCommand = {
+      email: email,
+      password: password
+    }
+
+    this.loginService.loginUser(data).subscribe(
+      (response) => {
+        if (parseInt(response) === 1) {
+          console.log('data 1 keldi');
+        }
+        else {
+          console.log('data 0 keldi');
+        }
+      }
+    )
+  }
+}
+export interface LoginCommand {
+  email: string,
+  password: string
 }
